780af8a5091031c2c64675a8ea95a859ec451958
nguyenvqtuan <157955370+nguyenvqtuan@users.noreply.github.com>
2026-02-09 13:33:28 +0700

    Feat/conf project (#1)
    * feat: Add config resources follow env
    
    * feat: Add docker init
    
    * feat: Run spotless all project
    
    ---------
    
    Co-authored-by: tuan.nguyenvan <nvqtuan@cmcglobal.vn>

--- src/main/java/com/example/lunar/LunarApplication.java
+++ src/main/java/com/example/lunar/LunarApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LunarApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LunarApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(LunarApplication.class, args);
+  }
 }
