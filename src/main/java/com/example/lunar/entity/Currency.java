622559312d769b75a3a201864a46f46766b7b2f6
nguyenvqtuan <157955370+nguyenvqtuan@users.noreply.github.com>
2026-02-09 14:35:18 +0700

    feat: Add entity for wallet api (#10)
    Co-authored-by: tuan.nguyenvan <nvqtuan@cmcglobal.vn>

---
+++ src/main/java/com/example/lunar/entity/Currency.java
@@ -0,0 +1,30 @@
+package com.example.lunar.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "currency")
+public class Currency {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String currency;
+
+  private LocalDateTime deletedDate;
+
+  private String deletedBy;
+
+  private LocalDateTime updatedDate;
+
+  private String updatedBy;
+
+  private LocalDateTime createdDate;
+
+  private String createdBy;
+}
---
+++ src/main/java/com/example/lunar/entity/Wallet.java
@@ -0,0 +1,43 @@
+package com.example.lunar.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "wallet")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Wallet {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private UUID userId;
+
+  private UUID currencyId;
+
+  private String status;
+
+  private BigDecimal balance;
+
+  private LocalDateTime deletedDate;
+
+  private String deletedBy;
+
+  private LocalDateTime updatedDate;
+
+  private String updatedBy;
+
+  private LocalDateTime createdDate;
+
+  private String createdBy;
+}
