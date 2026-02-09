d1610320369301d4ad48af7ac00eb15229d48ea9
tuan.nguyenvan <nvqtuan@cmcglobal.vn>
2026-02-09 11:21:31 +0700

    feat: Init Lunar project

---
+++ src/test/java/com/example/lunar/LunarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
