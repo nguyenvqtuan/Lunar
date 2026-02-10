fbda9cf84b40fa9e6c1a7ecabfef855671879215
tuan.nguyenvan <nvqtuan@cmcglobal.vn>
2026-02-10 13:27:02 +0700

    feat: Run spotless

--- src/main/java/com/example/lunar/common/filter/TraceFilter.java
+++ src/main/java/com/example/lunar/common/filter/TraceFilter.java
@@ -4,23 +4,24 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.UUID;
-
 @Component
 public class TraceFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            MDC.put("traceId", UUID.randomUUID().toString());
-            MDC.put("requestId", request.getHeader("X-Request-Id"));
-            filterChain.doFilter(request, response);
-        } finally {
-            MDC.clear();
-        }
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      MDC.put("traceId", UUID.randomUUID().toString());
+      MDC.put("requestId", request.getHeader("X-Request-Id"));
+      filterChain.doFilter(request, response);
+    } finally {
+      MDC.clear();
     }
+  }
 }
