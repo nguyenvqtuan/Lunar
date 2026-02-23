09f5bfabc9a9ab9befcf48da83bd104a0436b8e8
tuannvq <nvtuanq123@gmail.com>
2026-02-23 15:27:01 +0700

    feat: Add get wallet api

--- src/main/java/com/example/lunar/dto/mapper/WalletMapper.java
+++ src/main/java/com/example/lunar/dto/mapper/WalletMapper.java
@@ -1,4 +1,18 @@
 package com.example.lunar.dto.mapper;
 
-// TODO: Add mapper from convert request to dto
-public interface WalletMapper {}
+import com.example.lunar.dto.command.WalletCommand;
+import com.example.lunar.dto.request.WalletRequest;
+import com.example.lunar.dto.response.WalletResponse;
+import com.example.lunar.entity.Wallet;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface WalletMapper {
+
+    WalletResponse toDto(Wallet wallet);
+
+    @Mapping(source = "userName", target = "userName")
+    @Mapping(source = "currency", target = "currency")
+    WalletCommand from(WalletRequest request);
+}
